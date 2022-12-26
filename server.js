09c6a0e5fbf7f649c8c2ad2e46f3040bb222ee7b
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2022-12-26 17:27:10 +0100

    express setup

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
