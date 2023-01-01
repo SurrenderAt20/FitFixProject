49928bc8483c71fc658ddcb719d4e488948df480
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-01-01 21:46:53 +0100

    handleWorkoutSelection

--- src/Screens/LoggedInScreens/YourWorkout.js
+++ src/Screens/LoggedInScreens/YourWorkout.js
@@ -9,6 +9,7 @@ export const YourWorkout = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [workoutProgram, setWorkoutProgram] = useState([]);
+  const [selectedWorkout, setSelectedWorkout] = useState(null);
 
   useEffect(() => {
     axios.get("http://localhost:3001/login").then((response) => {
@@ -27,25 +28,41 @@ export const YourWorkout = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  const handleWorkoutSelection = (workoutID) => {
+    axios.get(`http://localhost:3001/api/workout/${workoutId}`).then((res) => {
+      setSelectedWorkout(res.data);
+    });
+  };
+
   return (
     <div>
       <LoggedInNav />
       {workoutProgram.map((exercise) => (
-        <div className="flex flex-row justify-center">
-          <div className="list__item__card" key={exercise.id}>
+        <div className="flex justify-center">
+          <div className="list__item__card flex flex-row" key={exercise.id}>
             <h3>
               <b>Name:</b>
               <br />
               {exercise.name}
             </h3>
-            <h4>
-              <b>Muscle Group:</b>
-              <br />
-              {exercise.muscle_category}
-            </h4>
+            <br />
+            <button
+              className="p-3 px-6 pt-2 mb-4 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+              onClick={handleWorkoutSelection}
+            >
+              Open
+            </button>
           </div>
         </div>
       ))}
+
+      {selectedWorkout && (
+        <div key={exercise.id}>
+          {JSON.parse(selectedWorkout.exercises).map((exercise) => (
+            <h3>{exercise.name}</h3>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
