92797c6a4c3651e1214b4a3130b9b6f53952a329
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-01-02 18:31:35 +0100

    challenge completed

--- src/Screens/LoggedInScreens/Challenge/Chart.js
+++ src/Screens/LoggedInScreens/Challenge/Chart.js
@@ -8,60 +8,24 @@ import {
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { useRef, useEffect } from "react";
+import "./Chart.css";
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
 export const Chart = ({ players, scores }) => {
-  const chartRef = useRef();
-
-  const data = {
-    labels: players.map((player) => player.name),
-    datasets: [
-      {
-        label: "Score",
-        data: scores,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
   return (
-    <div>
-      <Bar
-        className="w-max items-center"
-        data={data}
-        options={{
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        }}
-      />
+    <div className="chart">
+      {players.map((player, index) => (
+        <div className="chart__bar" key={player.name}>
+          <div
+            className="chart__bar-value"
+            style={{ height: `${scores[index] * 10}px` }}
+          >
+            {scores[index]}
+          </div>
+          <div className="chart__bar-label">{player.name}</div>
+        </div>
+      ))}
     </div>
   );
 };
-
-/* [
-    {
-      ticks: {
-        min: 0,
-      },
-    },
-  ], */
